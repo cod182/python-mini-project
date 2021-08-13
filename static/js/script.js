668912ca9b1bb204213a0b6c989c80e6502e465a
Codie Stephens-Evans <codie18@gmail.com>
2021-08-13 13:17:15 +0000

    Add task collapsible set up

--- static/js/script.js
+++ static/js/script.js
@@ -1,4 +1,12 @@
 $(document).ready(function(){
     $('.sidenav').sidenav({edge:"right"});
+    $('.collapsible').collapsible();
   });
         
+
+// document.addEventListener('DOMContentLoaded', function() {
+//   var elems = document.querySelectorAll('.collapsible');
+//   var instances = M.Collapsible.init(elems, options);
+//   var elems = document.querySelectorAll('.sidenav');
+//   var instances = M.Sidenav.init(elems, options);
+// });
