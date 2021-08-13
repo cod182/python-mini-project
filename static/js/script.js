39fcee9e6bf7a8eeb7c37195709065b0aa3b46b8
Codie Stephens-Evans <codie18@gmail.com>
2021-08-13 13:37:20 +0000

    bind data from db to accordian

--- static/js/script.js
+++ static/js/script.js
@@ -1,12 +1,15 @@
 $(document).ready(function(){
     $('.sidenav').sidenav({edge:"right"});
     $('.collapsible').collapsible();
+    $('.tooltipped').tooltip();
   });
         
 
 // document.addEventListener('DOMContentLoaded', function() {
-//   var elems = document.querySelectorAll('.collapsible');
-//   var instances = M.Collapsible.init(elems, options);
-//   var elems = document.querySelectorAll('.sidenav');
-//   var instances = M.Sidenav.init(elems, options);
+//     var elems = document.querySelectorAll('.collapsible');
+//     var instances = M.Collapsible.init(elems, options);
+//     var elems = document.querySelectorAll('.sidenav');
+//     var instances = M.Sidenav.init(elems, options);
+//     var elems = document.querySelectorAll('.tooltipped');
+//     var instances = M.Tooltip.init(elems, options);
 // });
