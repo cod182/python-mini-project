e33ca734bab2304d4d6d4a167fce13d444d30a1e
Codie Stephens-Evans <codie18@gmail.com>
2021-08-13 14:06:00 +0000

    add taks form input fields

--- static/js/script.js
+++ static/js/script.js
@@ -1,7 +1,15 @@
 $(document).ready(function(){
-    $('.sidenav').sidenav({edge:"right"});
+    $('.sidenav').sidenav({edge:'right'});
     $('.collapsible').collapsible();
     $('.tooltipped').tooltip();
+    $('.datepicker').datepicker({
+      format: 'dd mmmm, yyyy',
+      yearRange: 3,
+      showClearBtn: true,
+      i18n: {
+        done: 'select'
+      }
+    });
   });
         
 
@@ -12,4 +20,6 @@ $(document).ready(function(){
 //     var instances = M.Sidenav.init(elems, options);
 //     var elems = document.querySelectorAll('.tooltipped');
 //     var instances = M.Tooltip.init(elems, options);
+//     var elems = document.querySelectorAll('.datepicker');
+//     var instances = M.Datepicker.init(elems, options);
 // });
